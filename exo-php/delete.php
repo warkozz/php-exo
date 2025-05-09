aad2ce40135c133edab28c0a7bd287d2f8aeb108
warkozz <hakimrayane@hotmail.fr>
2025-05-09 13:45:57 +0200

    fix: Correct spelling in the delete confirmation header

--- exo-php/delete.php
+++ exo-php/delete.php
@@ -48,7 +48,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="mt-5 mb-3">Supprimer l'enregistremnt</h2>
+                    <h2 class="mt-5 mb-3">Supprimer l'enregistrement</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
                             <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
